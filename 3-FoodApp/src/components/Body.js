0a80d72513a5a19acfeae06d3de57278e49d76a7
Haren-Sharma <harensharma33@gmail.com>
2025-08-24 19:32:41 +0530

    Lec-6

--- 3-FoodApp/src/components/Body.js
+++ 3-FoodApp/src/components/Body.js
@@ -1,9 +1,17 @@
 import RestaurantCard from "./RestaurantCard";
 import res from "../utils/mockData";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Body = () => {
-  const [restaurants, setRestaurants] = useState(res);
+  const [restaurants, setRestaurants] = useState([]);
+  const [filteredListRestaurants, setFilteredList] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    setRestaurants(res);
+    setFilteredList(res);
+    //we can use an external api as well
+  }, []);
   /*
   Body
   -Search
@@ -13,32 +21,38 @@ const Body = () => {
   const filterTopRatedRes = (e) => {
     if (e.target.classList?.contains("btn-filter-selected")) {
       e.target.classList.remove("btn-filter-selected");
-      setRestaurants(res);
+      setFilteredList(restaurants);
     } else {
       e.target.classList.add("btn-filter-selected");
-      setRestaurants((prev) => {
-        return prev.filter((r) => r.rating > 4.4);
+      setFilteredList((prev) => {
+        return restaurants.filter((r) => r.rating > 4.4);
       });
     }
   };
-  const search = (text) => {
-    setRestaurants((prev) => {
-      return prev.filter((res) => res.name.includes(text));
-    });
+  const search = () => {
+    const filtered = restaurants?.filter((res) =>
+      res.name?.toLowerCase().includes(searchText.toString())
+    );
+    setFilteredList(filtered);
   };
   return (
     <div className="body">
-      <input
-        type="text"
-        id="search-box"
-        placeholder="Search any restaurant"
-        onChange={(e) => search(e.target.value)}
-      />
-      <button id="filter-top-rated" onClick={filterTopRatedRes}>
-        Top Rated
-      </button>
+      <div className="body-header-container">
+        <input
+          style={{ paddingLeft: 20, paddingRight: 20 }}
+          type="text"
+          value={searchText}
+          onChange={(e) => setSearchText(e.target.value)}
+        />
+        <button id="filter-top-rated" onClick={search}>
+          Search
+        </button>
+        <button id="filter-top-rated" onClick={filterTopRatedRes}>
+          Top Rated
+        </button>
+      </div>
       <div id="restaurant-container">
-        {restaurants.map((restaurant) => (
+        {filteredListRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.id} data={restaurant} />
         ))}
       </div>

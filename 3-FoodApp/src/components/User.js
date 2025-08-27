ee7ec1b536d7fce12a5f6929de3ed690b849ca00
Haren-Sharma <harensharma33@gmail.com>
2025-08-27 17:53:14 +0530

    Lec-8: making state variables in class based components

--- 3-FoodApp/src/components/User.js
+++ 3-FoodApp/src/components/User.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 const User = ({ name, twid }) => {
+  const [count, setCount] = useState(0);
+  const [count1, setCount1] = useState(1);
   return (
     <div className="user-card">
       <h2>{name}</h2>
       <h3>{twid}</h3>
+      <div>Count : {count}</div>
+      <div>Count : {count1}</div>
     </div>
   );
 };
--- 3-FoodApp/src/components/UserClass.js
+++ 3-FoodApp/src/components/UserClass.js
@@ -2,16 +2,27 @@ import React from "react";
 
 class UserClass extends React.Component {
   constructor(props) {
+    /*
+      The below line ensures that this.props is properly initialized
+      With the help of this we can access this.props in our constructor as well
+      */
     super(props);
-    //The above line ensures that this.props is properly initialized
-    //With the help of this we can access this.props in our constructor as well
-    console.log("🚀 ~ UserClass ~ constructor ~ props:", props);
+    this.state = {
+      //this will create the state variables for this component
+      count: 110,
+      count1: 10,
+    };
   }
   render() {
+    const { name, twid } = this.props;
+    const { count, count1 } = this.state;
+
     return (
       <div className="user-card">
-        <h2>{this.props.name}</h2>
-        <h3>{this.props.twid}</h3>
+        <h2>{name}</h2>
+        <h3>{twid}</h3>
+        <div>Count : {count}</div>
+        <div>Count : {count1}</div>
       </div>
     );
   }

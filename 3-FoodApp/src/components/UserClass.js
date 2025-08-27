bb646e814070ee9f37c7530ff3e18ceec1cbc773
Haren-Sharma <harensharma33@gmail.com>
2025-08-27 18:09:45 +0530

    Lec-8: Updating the state variables in class based components

--- 3-FoodApp/src/components/UserClass.js
+++ 3-FoodApp/src/components/UserClass.js
@@ -9,7 +9,7 @@ class UserClass extends React.Component {
     super(props);
     this.state = {
       //this will create the state variables for this component
-      count: 110,
+      count: 10,
       count1: 10,
     };
   }
@@ -17,11 +17,25 @@ class UserClass extends React.Component {
     const { name, twid } = this.props;
     const { count, count1 } = this.state;
 
+    const increaseTheCount = () => {
+      this.setState({
+        /*
+            NEVER UPDATE STATE VARIABLES DIRECTLY    
+            With the hepl of this function we can update our state variables
+            It will update only those variables mentioned , will not touch other
+            Here below as we have updated only the value of count variable
+            it won't hamper the value for count1 variable
+            */
+        count: this.state.count + 1,
+      });
+    };
+
     return (
       <div className="user-card">
         <h2>{name}</h2>
         <h3>{twid}</h3>
         <div>Count : {count}</div>
+        <button onClick={increaseTheCount}>Increase the Count</button>
         <div>Count : {count1}</div>
       </div>
     );

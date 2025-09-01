b8d94205049018164c5adbeb55115d1cf010ff14
Haren-Sharma <harensharma33@gmail.com>
2025-09-01 21:17:51 +0530

    Lec-11: Inserted a Add button ui

--- 3-FoodApp/src/components/RestaurantCategoryItem.js
+++ 3-FoodApp/src/components/RestaurantCategoryItem.js
@@ -1,16 +1,21 @@
-import React from 'react'
+import React, { useState } from "react";
 
-const RestaurantCategoryItem = ({name,description,price,image}) => {
+const RestaurantCategoryItem = ({ name, description, price, image }) => {
   return (
-    <div className='flex justify-between items-center p-4 mb-4 border-1 rounded-xl'>
-    <div>
-      <div className='font-semibold'>{name}</div>
-      <div className='italic mt-4'>{description}</div>
-      <div className='font-bold'>{"$"+price}</div>
+    <div className="flex justify-between items-center p-2 mb-4 border-1 rounded-xl">
+      <div>
+        <div className="font-semibold">{name}</div>
+        <div className="italic mt-4">{description}</div>
+        <div className="font-bold">{"$" + price}</div>
+      </div>
+      <div className="relative p-2">
+        <div className="absolute bg-black text-white rounded-xl px-3 py-1 bottom-0 right-12">
+          Add +
+        </div>
+        <img className="w-40 rounded-xl" src={image} />
+      </div>
     </div>
-    <img className='w-40 rounded-xl' src={image}/>
-    </div>
-  )
-}
+  );
+};
 
-export default RestaurantCategoryItem
+export default RestaurantCategoryItem;

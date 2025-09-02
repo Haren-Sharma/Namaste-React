433015ead0f91c1fcc5fa427c652bab7422eb105
Haren-Sharma <harensharma33@gmail.com>
2025-09-02 15:54:11 +0530

    Lec-12: configured redux store

--- 3-FoodApp/src/AppLayout.js
+++ 3-FoodApp/src/AppLayout.js
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import { Outlet } from "react-router";
 import UserContextProvider from "./context/UserContextProvider";
+import { Provider } from "react-redux";
+import store from "./redux/store/store";
 
 const AppLayout = () => {
   const [darkMode, setDarkMode] = useState(false);
@@ -27,16 +29,18 @@ const AppLayout = () => {
         -Restaurant Cards
     */
   return (
-    <UserContextProvider>
-      <Header setDarkMode={setDarkMode} />
-      <Outlet />
-      {/*
+    <Provider store={store}>
+      <UserContextProvider>
+        <Header setDarkMode={setDarkMode} />
+        <Outlet />
+        {/*
       This Outlet component is provided by react-router
       With the help of this , the children routes created in the route configuration(createBrowserRouter)
       will get replaced with Outlet, depending upon the path given to the children.
       This won't affect our Header it will remian intact in it's position
       */}
-    </UserContextProvider>
+      </UserContextProvider>
+    </Provider>
   );
 };
 export default AppLayout;
---
+++ 3-FoodApp/src/redux/store/store.js
@@ -0,0 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import cartslice from "../features/CartSlice";
+
+const store=configureStore({
+    reducer:{
+        cart:cartslice
+    }
+})
+
+export default store;
